f395835b0c1f4c8f862776e22abfe22a83cf1aed
Viktor Vinogradov <viktor.vinogradov@list.ru>
2024-04-08 21:08:03 +0300

    Add sem1 + HW

---
+++ Seminar1/src/seminars/first/Calculator/Calculator.java
@@ -0,0 +1,57 @@
+package seminars.first.Calculator;
+
+public class Calculator {
+
+    // Нужно написать в калькуляторе метод вычисления суммы покупки со скидкой и проверить его, используя AssertJ
+    // Примерная сигнатура и тело метода:
+    public static double calculatingDiscount(double purchaseAmount, int discountAmount) {
+        // purchaseAmount - сумма покупки
+        // discountAmount - размер скидки в %
+        // Метод должен возвращать сумму покупки со скидкой
+        if (discountAmount < 0) {
+            throw new ArithmeticException("Скидка не может быть отрицательной!");
+        } else if (discountAmount >= 100) {
+            throw new ArithmeticException("Скидка 100%! Магазин себе такого позволить не может! :)");
+        }
+        return purchaseAmount - (purchaseAmount * discountAmount / 100);
+    }
+
+    public static int calculation(int firstOperand, int secondOperand, char operator) {
+        int result;
+
+        switch (operator) {
+            case '+':
+                result = firstOperand + secondOperand;
+                break;
+            case '-':
+                result = firstOperand - secondOperand;
+                break;
+            case '*':
+                result = firstOperand * secondOperand;
+                break;
+            case '/':
+                if (secondOperand != 0) {
+                    result = firstOperand / secondOperand;
+                    break;
+                } else {
+                    throw new ArithmeticException("Division by zero is not possible");
+                }
+            default:
+                throw new IllegalStateException("Unexpected value operator: " + operator);
+        }
+        return result;
+    }
+
+    // HW1.1: Придумайте и опишите (можно в псевдокоде) функцию извлечения корня и
+    // необходимые проверки для него используя граничные случаи
+    public static double squareRootExtraction(double num) {
+        //  0
+        //  Отрицательные числа
+        //  Дробные значения корней
+        //  Целые
+            if(num < 0) {
+                throw new IllegalArgumentException("Cannot calculate square root of a negative number");
+            }
+            return Math.sqrt(num);
+    }
+}
